d76d45322b39ccaae0b11b6e7c5cf8e3c0f98790
tungxuando <72494652+tungxuando@users.noreply.github.com>
2021-05-24 11:55:52 +0200

    Update Challenge

--- Solutions/Challenge1-5.py
+++ Solutions/Challenge1-5.py
@@ -2,7 +2,6 @@ from djitellopy import tello
 
 from time import sleep
 
-
 def challenge1():
     me = tello.Tello()
 
@@ -22,7 +21,6 @@ def challenge1():
 
     me.land()
 
-
 def challenge2():
     me = tello.Tello()
 
@@ -39,7 +37,6 @@ def challenge2():
     sleep(2)
 
     me.land()
-
 
 def challenge3():
     me = tello.Tello()
@@ -60,7 +57,6 @@ def challenge3():
 
     me.land()
 
-
 def challenge4():
     me = tello.Tello()
 
@@ -72,51 +68,23 @@ def challenge4():
 
     me.send_rc_control(0, 0, 50, 0)
     """up 50"""
-    sleep(2)
-
-    me.send_rc_control(0, 25, 0, 0)
-    """forward 25"""
-
-    sleep(2)
-
-    me.send_rc_control(0, 0, 0, 50)
-
-    """turn clockwise 50"""
-
-    sleep(2)
+    sleep(1)
 
     me.send_rc_control(0, 50, 0, 0)
-
-    """forward 50"""
-
-    sleep(2)
-
-    me.send_rc_control(0, 0, 0, 50)
-
-    """turn clockwise 50"""
-
-    sleep(2)
-
-    me.send_rc_control(0, 25, 0, 0)
-
     """forward 25"""
-
     sleep(2)
 
-    me.send_rc_control(0, 0, 0, 50)
-    """turn clock wise 50"""
+    me.send_rc_control(50, 0, 0, 0)
+    sleep(1)
 
+    me.send_rc_control(0, -50, 0, 0)
     sleep(2)
 
-    me.send_rc_control(0, 50, 0, 0)
-    """forward 50"""
+    me.send_rc_control(-50, 0, 0, 0)
 
-    sleep(2)
-
-    me.send_rc_control()
+    sleep(1)
 
     me.land()
-
 
 def challenge5():
     me.connect()
@@ -134,3 +102,5 @@ def challenge5():
     sleep(2)
 
     me.land()
+
+challenge2()
