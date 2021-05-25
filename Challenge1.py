9fe7c656ee1b16e73e58296816bc4cb404514d5b
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-25 15:44:55 +0100

    print changed position

--- Challenge1.py
+++ Challenge1.py
@@ -6,17 +6,19 @@ from time import sleep
 def challenge1Test():
     me = tello.Tello()
 
+    print("I am working?")
+    print("If that is the case please print me!")
+    print("hello world! This is a test to see if the docker container is going to make the code work")
     me.connect()
     if me.get_battery() < 30:
         return "hello world! This is a test to see if the docker container is going to make the code work"
 
-    print("I am working?")
     me.takeoff()
 
     me.send_rc_control(0, 0, 50, 0)
 
     sleep(2)
-    print("If that is the case please print me!")
+
     me.send_rc_control(0, 50, 0, 0)
 
     sleep(2)
