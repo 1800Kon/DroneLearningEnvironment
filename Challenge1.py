0d158ed8c849656a7ed6ded5e082e8cd65a6dcea
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-25 15:50:46 +0100

    print changed position

--- Challenge1.py
+++ Challenge1.py
@@ -2,13 +2,13 @@ from djitellopy import tello
 
 from time import sleep
 
+print("I am working?")
+print("If that is the case please print me!")
+print("hello world! This is a test to see if the docker container is going to make the code work")
 
 def challenge1Test():
     me = tello.Tello()
 
-    print("I am working?")
-    print("If that is the case please print me!")
-    print("hello world! This is a test to see if the docker container is going to make the code work")
     me.connect()
     if me.get_battery() < 30:
         return "hello world! This is a test to see if the docker container is going to make the code work"
