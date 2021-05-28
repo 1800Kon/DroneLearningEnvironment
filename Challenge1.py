d2bb24a243b886d0f8ea5ac4583113fe28c04457
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2021-05-28 13:29:04 +0200

    Flake8 installation,test if it works

--- Challenge1.py
+++ Challenge1.py
@@ -2,26 +2,28 @@ from djitellopy import tello
 
 from time import sleep
 
-print("I am working?")
-print("If that is the case please print me!")
-print("hello world! This is a test to see if the docker container is going to make the code work")
+me = tello.Tello()
 
-def challenge1Test():
-    me = tello.Tello()
 
-    me.connect()
-    if me.get_battery() < 30:
-        return "hello world! This is a test to see if the docker container is going to make the code work"
 
-    me.takeoff()
 
-    me.send_rc_control(0, 0, 50, 0)
 
-    sleep(2)
 
-    me.send_rc_control(0, 50, 0, 0)
 
-    sleep(2)
 
-    me.land()
 
+
+
+me.connect()
+print('test 1')
+me.takeoff()
+
+me.send_rc_control(0, 0, 50, 0)
+
+sleep(2)
+print('test 2')
+me.send_rc_control(0, 50, 0, 0)
+
+sleep(2)
+
+me.land()
