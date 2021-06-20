22221ef573fee81cbacf6fdfc57b2dc7368ae561
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-06-20 16:16:25 +0100

    challenge 2 included

--- Challenge/Challenges.py
+++ Challenge/Challenges.py
@@ -5,16 +5,15 @@ from djitellopy import tello
 me = tello.Tello()
 
 me.connect()
-print('test 1')
+
+print(me.get_battery())
+
 me.takeoff()
 
-me.send_rc_control(0, 0, 50, 0)
+sleep(2)
 
-sleep(2)
-print('test 2')
-me.send_rc_control(0, 50, 0, 0)
-sleep(2)
-me.send_rc_control(0, 50, 0, 0)
+me.send_rc_control(0, 0, 0, 50)
+
 sleep(2)
 
 me.land()
