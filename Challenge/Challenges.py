aa0ff0b55e5dfb7b97d77d70adc36e4b1d5334fc
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-06-20 16:58:41 +0100

    added challenge 1 again

--- Challenge/Challenges.py
+++ Challenge/Challenges.py
@@ -1,1 +1,23 @@
-print("hello world")
+from time import sleep
+
+from djitellopy import tello
+
+me = tello.Tello()
+
+me.connect()
+
+print(me.get_battery())
+
+me.takeoff()
+
+sleep(1)
+
+me.send_rc_control(0, 50, 0, 0)
+
+sleep(2)
+
+me.send_rc_control(0, -50, 0, 0)
+
+sleep(2)
+
+me.land()
